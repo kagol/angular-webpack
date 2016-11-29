75808421a1caccbb349cf082ce56bd21d10f58ab
kagol <kagol@sina.com>
2016-11-29 16:09:57 +0800

    add config file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  context: __dirname + '/app',//上下文
+  entry: './index.js',//入口文件
+  output: {//输出文件
+    path: __dirname + '/app',
+    filename: './bundle.js'
+  },
+  module: {
+    loaders: [//加载器
+      {test: /\.html$/, loader: 'raw'},
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.(png|jpg|ttf)$/, loader: 'url?limit=8192'}
+    ]
+  }
+};
